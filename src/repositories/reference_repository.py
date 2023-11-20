23934d4124526ebbd39dc4b7800b05d46b2fb9a0
gitelras <ella.rasalo@helsinki.fi>
2023-11-20 17:44:46 +0200

    adding book.py Co-authored-by: ounisk ounisk@users.noreply.github.comCo-authored-by: ravioli lauri.vilamo@helsinki.fi

---
+++ src/repositories/reference_repository.py
@@ -0,0 +1,37 @@
+from entities.book import Book
+
+class Reference_Repository:
+    def __init__(self):
+        self._references = []
+
+    def find_all(self):
+        return self._references
+
+    #def find_by_username(self, username):
+      #  users = self.find_all()
+
+       # users_with_username = filter(
+       #     lambda user: user.username == username,
+       #     users
+       # )
+
+      #  users_with_username_list = list(users_with_username)
+
+     #   return users_with_username_list[0] if len(users_with_username_list) > 0 else None
+
+
+def create(self, book):
+        references = self.find_all()
+
+        #existing_user = self.find_by_username(user.username)
+
+        #if existing_user:
+         #   raise Exception(
+         #       f"User with username {user.username} already exists"
+         #   )
+
+        references.append(book)
+
+        self._references = references
+
+        return book
