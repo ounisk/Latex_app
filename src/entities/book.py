934b7f5d13cc78e91e1931ff425c3e6de751ea2f
gitelras <ella.rasalo@helsinki.fi>
2023-11-20 17:25:52 +0200

    adding book.py Co-authored-by: ounisk ounisk@users.noreply.github.comCo-authored-by: ravioli lauri.vilamo@helsinki.fi

---
+++ src/entities/book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, author:str, name:str, year:int, publisher:str):
+        self.author = author
+        self.name = name
+        self.year = year
+        self.publisher = publisher
+
+
