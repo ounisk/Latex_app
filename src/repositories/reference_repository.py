50ee444693a8dfe65e45157a843cb88dfe8a81eb
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-25 11:35:54 +0200

    Add .csv file functionalities

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -1,37 +1,68 @@
-#from entities.book import Book
-#
+from entities.book import Book
+from pathlib import Path #
+from config import ALIST_FILE_PATH
+
+
+
 class ReferenceRepository:
-    def __init__(self):
-        self._references = []
+    def __init__(self, file_path):
+        self._file_path = file_path
+        #self._references = []
+
+
+    def _check_file(self):
+        Path(self._file_path).touch()    
+
+
+    def _get_list(self):
+        references = []
+
+        self._check_file()
+        with open(self._file_path, encoding="utf-8") as file:
+            for row in file:
+                #print(row)
+                row = row.replace("\n", "")
+                parts = row.split(";")
+                #print("Osat:")
+                #print(parts)
+                author = parts[0]
+                name = parts[1]
+                year = parts[2]
+                publisher = parts[3]
+
+                references.append(Book(author, name, year, publisher))
+
+        return references
+
 
     def find_all(self):
-        return self._references
-
-    #def find_by_username(self, username):
-      #  users = self.find_all()
-
-       # users_with_username = filter(
-       #     lambda user: user.username == username,
-       #     users
-       # )
-
-      #  users_with_username_list = list(users_with_username)
-
-     #   return users_with_username_list[0] if len(users_with_username_list) > 0 else None
+        #return self._references
+        return self._get_list() 
 
 
     def create(self, book):
         references = self.find_all()
-
-        #existing_user = self.find_by_username(user.username)
-
-        #if existing_user:
-         #   raise Exception(
-         #       f"User with username {user.username} already exists"
-         #   )
-
         references.append(book)
 
-        self._references = references
-
+        #self._references = references
+        self._update(references)   #uuden kirjan jälkeen updatetaan file
         return book
+
+
+    def delete_all(self):    #tyhjentää koko listan, hyödyksi esim. testaamisessa
+        list_references = []
+        self._update(list_references)
+
+
+
+    def _update(self, references):   #päivitää filen uusilla tiedoilla
+        self._check_file()
+
+        with open(self._file_path, "w", encoding="utf-8") as file:
+            for ref in references:
+
+                row = f"{ref.author};{ref.name};{ref.year};{ref.publisher}" #fileen lisätty tieto tulee str:nä
+                file.write(row+"\n")
+
+
+reference_repository = ReferenceRepository(ALIST_FILE_PATH)
