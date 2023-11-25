125decdf2f4eb911dc2e666001af6401336cbc5b
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-25 11:38:47 +0200

    Make minor refactoring

--- src/index.py
+++ src/index.py
@@ -1,11 +1,11 @@
-from repositories.reference_repository import ReferenceRepository
+#from repositories.reference_repository import ReferenceRepository
 from services.reference_services import ReferenceService
 from console_io import ConsoleIO
 from app import App
 
 def main():
-    reference_repo = ReferenceRepository()
-    service_ref = ReferenceService(reference_repo)
+    #reference_repo = ReferenceRepository()
+    service_ref = ReferenceService() #(reference_repo)
     console_io = ConsoleIO()
     app = App(service_ref, console_io)
 
