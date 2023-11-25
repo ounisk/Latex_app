e88a9b4d35ac52ba75c4db678a37b7fa8b6b563d
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-25 11:37:48 +0200

    Refactor tests

--- src/tests/reference_services_test.py
+++ src/tests/reference_services_test.py
@@ -1,17 +1,18 @@
 import unittest
 from services.reference_services import ReferenceService
 from entities.book import Book
-from repositories.reference_repository import ReferenceRepository
+#from repositories.reference_repository import ReferenceRepository
 
 
 class TestReferenceService(unittest.TestCase): 
     def setUp(self):
-        self.reference_service = ReferenceService(ReferenceRepository())
-        self.ref_book1 = Book("Waltari", "Sinuhe", 1900, "Otava")
+        self.reference_service = ReferenceService() #(ReferenceRepository())
+        #self.ref_book1 = Book("Waltari", "Sinuhe", "1900", "Otava")
 
 
     def test_adding_book_reference_is_on_the_list(self):
-        self.reference_service.create_ref("Waltari", "Sinuhe", 1900, "Otava")
+        self.reference_service.delete_all_references()
+        self.reference_service.create_ref("Waltari", "Sinuhe", "1900", "Otava")  #huom! vuosi pitää laitaa str-muodossa, koska on filessä niin
 
         references = self.reference_service.print_refs()
-        self.assertEqual((references[0].author, references[0].name, references[0].year), ("Waltari", "Sinuhe", 1900))
+        self.assertEqual((references[0].author, references[0].name, references[0].year, references[0].publisher), ("Waltari", "Sinuhe", "1900", "Otava"))
