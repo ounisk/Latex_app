3eb6a928e030d2ad6028dd8e63e3fad00b0b3259
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-26 16:43:29 +0200

    Add reposit.funcs for article and inproceedings

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from entities.book import Book
+from entities.book import Book, Article, InProceedings
 from config import ALIST_FILE_PATH
 
 
@@ -19,18 +19,36 @@ class ReferenceRepository:
         self._check_file()
         with open(self._file_path, encoding="utf-8") as file:
             for row in file:
-                #print(row)
                 row = row.replace("\n", "")
                 parts = row.split(";")
-                #print("Osat:")
-                #print(parts)
-                author = parts[0]
-                name = parts[1]
-                year = parts[2]
-                publisher = parts[3]
 
-                references.append(Book(author, name, year, publisher))
+                if parts[0]== "book": #
+                    ref_type = parts[0]   #
+                    author = parts[1]
+                    name = parts[2]
+                    year = parts[3]
+                    publisher = parts[4]
 
+                    references.append(Book(ref_type, author, name, year, publisher))
+
+                if parts[0] == "article":
+                    ref_type = parts[0]   #
+                    author = parts[1]
+                    title = parts[2]
+                    journal = parts[3]
+                    year = parts[4]
+
+                    references.append(Article(ref_type, author, title, journal, year))
+
+                if parts[0] == "inproceedings":
+                    ref_type = parts[0]
+                    author = parts[1]
+                    title = parts[2]
+                    book_title = parts[3]
+                    publisher = parts[4]
+                    year = parts[5]
+
+                    references.append(InProceedings(ref_type, author, title, book_title, publisher, year))
         return references
 
 
@@ -59,9 +77,16 @@ class ReferenceRepository:
 
         with open(self._file_path, "w", encoding="utf-8") as file:
             for ref in references:
+                if ref.ref_type == "book":
+                    row = f"{ref.ref_type};{ref.author};{ref.name};{ref.year};{ref.publisher}"
+                    file.write(row+"\n")
+                if ref.ref_type == "article":
+                    row = f"{ref.ref_type};{ref.author};{ref.title};{ref.journal};{ref.year}"
+                    file.write(row+"\n")
+                if ref.ref_type == "inproceedings":
+                    row = f"{ref.ref_type};{ref.author};{ref.title};{ref.book_title};{ref.publisher};{ref.year}"
+                    file.write(row+"\n")
 
-                row = f"{ref.author};{ref.name};{ref.year};{ref.publisher}"
-                file.write(row+"\n")
              #fileen lisätty tieto tulee str:nä
 
 reference_repository = ReferenceRepository(ALIST_FILE_PATH)
