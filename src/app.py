9e8e2ca2eac29dd708d073d444dc5cbfb0c777b0
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-27 18:12:24 +0200

    Add create bib function

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ class App:
     def run(self):
         self.io.write("Welcome to Latex app")
         while True:
-            command = self.io.read(f"Choose command (add or print): {self.space:30}")  # Lisää vaihtoehtoja myöhemmin
+            command = self.io.read(f"Choose command (add, print, create bib): {self.space:30}")  # Lisää vaihtoehtoja myöhemmin
 
             if not command:
                 break
@@ -19,31 +19,37 @@ class App:
                     break
 
                 if ref_type == "book":
-                    (author, title, year, publisher) = self._read_book()
+                    (bib_ref, author, title, year, publisher) = self._read_book()
                 
-                    self.reference_services.create_ref_book(author, title, year, publisher)
+                    self.reference_services.create_ref_book(bib_ref, author, title, year, publisher)
 
                     self.io.write("Reference added")
 
                 elif ref_type == "article":
-                    (author, title, journal, year) = self._read_article()
+                    (bib_ref, author, title, journal, year) = self._read_article()
 
-                    self.reference_services.create_ref_article(author, title, journal, year)
+                    self.reference_services.create_ref_article(bib_ref, author, title, journal, year)
 
                     self.io.write("Reference added")
 
                 elif ref_type == "inproceedings":
-                    (author, title, book_title, publisher, year) = self._read_inproceedings()
+                    (bib_ref, author, title, book_title, publisher, year) = self._read_inproceedings()
 
-                    self.reference_services.create_ref_inpro(author, title, book_title, publisher, year)
+                    self.reference_services.create_ref_inpro(bib_ref, author, title, book_title, publisher, year)
 
                     self.io.write("Reference added")
 
             elif command == "print":
                 ref_list = self.reference_services.print_refs()
-                print("***VIITTEET***")      
+                print("***REFERENCES***")      
                 for ref in ref_list:   #siirretty tulostus tänne services:in puolelta
                    print(ref)
+
+            elif command == "create bib":
+                self.reference_services.create_bib_format_file()
+                print(".bib file has been created")
+
+
 
 
     def _read_book(self):
@@ -51,16 +57,18 @@ class App:
         title = self.io.read(f"Add title: {self.space:50}")
         year = self.io.read(f"Add publication date: {self.space:39}")
         publisher = self.io.read(f"Add publisher: {self.space:46}")
+        bib_ref = self.io.read(f"Add short bib-reference: {self.space:46}")
 
-        return (author, title, year, publisher)
+        return (bib_ref, author, title, year, publisher)
     
     def _read_article(self):
         author = self.io.read(f"Add author: {self.space:49}")
         title = self.io.read(f"Add title: {self.space:50}")
         journal = self.io.read(f"Add journal: {self.space:48}")
         year = self.io.read(f"Add publication date: {self.space:39}")
+        bib_ref = self.io.read(f"Add short bib-reference: {self.space:46}")
 
-        return (author, title, journal, year)
+        return (bib_ref, author, title, journal, year)
 
     def _read_inproceedings(self):
         author = self.io.read(f"Add author: {self.space:49}")
@@ -68,5 +76,6 @@ class App:
         book_title = self.io.read(f"Add book title: {self.space:45}")
         publisher = self.io.read(f"Add publisher: {self.space:46}")
         year = self.io.read(f"Add publication date: {self.space:39}")
+        bib_ref = self.io.read(f"Add short bib-reference: {self.space:46}")
 
-        return (author, title, book_title, publisher, year)
+        return (bib_ref, author, title, book_title, publisher, year)
