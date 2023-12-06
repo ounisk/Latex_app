41c724375ba3d3b1fda2e21cb5370c9fd9e4b67e
Pseudo Name <122291857+ravioli-maul@users.noreply.github.com>
2023-12-06 23:55:13 +0200

    Add more repository unit tests

--- src/tests/reference_repository_test.py
+++ src/tests/reference_repository_test.py
@@ -2,13 +2,14 @@ import unittest
 import tempfile
 import os
 from repositories.reference_repository import ReferenceRepository
+from entities.book import Book
 
 class TestReferenceRepository(unittest.TestCase):
 
     def setUp(self):
         self.test_file = tempfile.NamedTemporaryFile(delete=False)
         self.test_file.close()
-        self.repository = ReferenceRepository(self.test_file.name, 'test_bib_file_path')
+        self.repository = ReferenceRepository(self.test_file.name, "test_bib_file_path")
 
     def tearDown(self):
         os.remove(self.test_file.name)
@@ -21,6 +22,13 @@ class TestReferenceRepository(unittest.TestCase):
         references = self.repository._get_list()
         self.assertEqual(len(references), 0)
 
+    def test_create(self):
+        new_book = Book("book", "Kalle", "Kallen OhTu seikkailut", "2023", "Kallen Omakustanne", "KA23")
+        self.repository.create(new_book)
+
+        references = self.repository.find_all()
+        self.assertTrue(any(book.author == new_book.author and book.name == new_book.name for book in references))
+
     def test_create_and_find_all(self):
         reference_data = "KA23;book;Kalle;Kallen OhTu seikkailut;2023;Kallen Omakustanne"
         with open(self.test_file.name, 'w', encoding="utf-8") as file:
@@ -32,3 +40,12 @@ class TestReferenceRepository(unittest.TestCase):
         reference = references[0]
         self.assertEqual(reference.author, "Kalle")
         self.assertEqual(reference.name, "Kallen OhTu seikkailut")
+
+    def test_delete_all(self):
+        book = Book("book", "Kalle", "Kallen OhTu seikkailut", "2023", "Kallen Omakustanne", "KA23")
+        self.repository.create(book)
+
+        self.repository.delete_all()
+
+        references = self.repository.find_all()
+        self.assertEqual(len(references), 0)
