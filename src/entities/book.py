4b294c51a44ebea5928c445d83c64c36ecc2b922
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-12-07 12:33:59 +0200

    changed name to title

--- src/entities/book.py
+++ src/entities/book.py
@@ -1,9 +1,9 @@
 class Book:
-    def __init__(self, ref_type:str, author:str, name:str, year:int, publisher:str, bib_ref:str):
+    def __init__(self, ref_type:str, author:str, title:str, year:int, publisher:str, bib_ref:str):
         self.bib_ref = bib_ref
         self.ref_type = ref_type
         self.author = author
-        self.name = name
+        self.title = title
         self.year = year
         self.publisher = publisher
 
@@ -11,7 +11,7 @@ class Book:
         spacing = 10
         ref_type = f"{'Type':>{spacing}}: {self.ref_type}"
         author = f"{'Author':>{spacing}}: {self.author}"
-        name = f"{'Title':>{spacing}}: {self.name}"
+        title = f"{'Title':>{spacing}}: {self.title}"
         publisher = f"{'Publisher':>{spacing}}: {self.publisher}"
         year = f"{'Year':>{spacing}}: {self.year}"
         bib_ref = f"{'Bib ref':>{spacing}}: {self.bib_ref}"
