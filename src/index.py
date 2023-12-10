8e8dee40c1a9a18a1f5a542ab150f84e17f334e2
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-10 16:23:36 +0200

    Remove comments

--- src/index.py
+++ src/index.py
@@ -1,11 +1,9 @@
-#from repositories.reference_repository import ReferenceRepository
 from services.reference_services import ReferenceService
 from console_io import ConsoleIO
 from app import App
 
 def main():
-    #reference_repo = ReferenceRepository()
-    service_ref = ReferenceService() #(reference_repo)
+    service_ref = ReferenceService()
     console_io = ConsoleIO()
     app = App(service_ref, console_io)
 
