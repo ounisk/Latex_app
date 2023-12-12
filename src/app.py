f3d97b3fe63146b33a32a6b6d2fa5a93ac6cfea3
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-12-12 16:07:45 +0200

    removed whitespace

--- src/app.py
+++ src/app.py
@@ -71,8 +71,6 @@ class App:
                             bibref_input = default_bibref
                         fields.append(bibref_input)
                         try:
-
-
                             self.reference_services.create_reference(ref_type, fields)
                             self.io.write(f"\nReference type {ref_type} added")
                         except Exception as error:
