b90c919aa62adbd9e6d9d4f086dea3f55785adf1
Pseudo Name <122291857+ravioli-maul@users.noreply.github.com>
2023-12-13 07:05:33 +0200

    additional reference_services unit tests

--- src/tests/reference_services_test.py
+++ src/tests/reference_services_test.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import Mock
+from datetime import datetime
 from services.reference_services import ReferenceService, UserInputError
 from entities.book import Book
 from entities.article import Article
@@ -45,10 +46,31 @@ class TestReferenceService(unittest.TestCase):
         self.assertEqual(self.mock_reference_repository.create.call_count, 2)
         self.reference_service.delete_reference('Au23')
         self.mock_reference_repository.delete_from_repository.assert_called_once_with('Au23')
-        
 
     def test_validate_year(self):
         with self.assertRaises(UserInputError) as context:
             self.reference_service.validate_year("-1")
         output = "Year has to be in the range of 0-2023. Please try again."
         self.assertEqual(str(context.exception), output)  
+
+    def test_validate_valid_years(self):
+        years = [1, 2023, 1900, 2000]
+        for year in years:
+            self.assertTrue(self.reference_service.validate_year(str(year)))
+
+    def test_validate_non_numeric_years(self):
+        years = ["OhTu", "123xyz", "!@#", " 1500", "{}[]"]
+        for year in years:
+            with self.assertRaises(UserInputError) as test:
+                self.reference_service.validate_year(year)
+            output = "Year has to be numeric."
+            self.assertEqual(str(test.exception), output)
+
+    def test_validate_years_outside_range(self):
+        years = [-1, 2123, -1000, 3000]
+        current_year = (datetime.now()).year
+        for year in years:
+            with self.assertRaises(UserInputError) as test:
+                self.reference_service.validate_year(str(year))
+            output = f"Year has to be in the range of 0-{current_year}. Please try again."
+            self.assertEqual(str(test.exception), output)
